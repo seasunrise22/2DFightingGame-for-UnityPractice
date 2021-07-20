99a5e046a01e3f2c2a05747a95b8932476f33f12
seasunrise22 <seasunrise22@gmail.com>
2021-07-20 19:45:07 +0900

    달리기 모션 추가, 각 모션 사이의 전환이 부드럽게 이어지도록 구현. idleChecker를 통한 Idle상태 감지 기능 구현.

--- Assets/Scripts/HeroKnightController.cs
+++ Assets/Scripts/HeroKnightController.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class HeroKnightController : MonoBehaviour
 {    
-    private bool isDead = false; // 사망여부    
-    private bool isGrounded = false; // 캐릭터가 점프했을 때 애니메이터에 Set시킬 상태.
-    private float jumpForce = 350f; // 위쪽 방향키 입력했을 때 캐릭터의 위쪽으로 작용할 힘.
-    private Vector2 upRightForce = new Vector2(350f, 300f); // 오른쪽위로 점프할 때 가할 힘
-    private Vector2 upLeftForce = new Vector2(-350f, 300f); // 왼쪽위로 점프할 때 가할 힘    
+    private bool isDead = false;        // 사망여부    
+    private bool isGrounded = false;    // 캐릭터가 점프했을 때 애니메이터에 Set시킬 점프 상태.
+    private bool isRun = false;         // 캐릭터를 좌우로 조작했을 때 애니메이터에 Set시킬 달리기 상태.
+    private float jumpForce = 350f;     // 위쪽 방향키 입력했을 때 캐릭터의 위쪽으로 작용할 힘.
+    private float idleChecker;           // 아무것도 입력하지 않았을 때의 행동(달리기->Idle)을 결정짓기 위한 타이머
+    private Vector2 upRightForce = new Vector2(350f, 300f);     // 오른쪽위로 점프할 때 가할 힘
+    private Vector2 upLeftForce = new Vector2(-350f, 300f);     // 왼쪽위로 점프할 때 가할 힘    
     private Vector2 horizontalMoveForce = new Vector2(8.0f, 0);   
 
-    private Rigidbody2D heroKnightRigidbody; // 리지드바디 접근용    
-    private Animator heroKnightAnimator; // 애니메이터 접근용
+    private Rigidbody2D heroKnightRigidbody;    // 리지드바디 접근용    
+    private Animator heroKnightAnimator;        // 애니메이터 접근용
 
     // Start is called before the first frame update
     void Start()
@@ -29,6 +31,13 @@ public class HeroKnightController : MonoBehaviour
         }
         else
         {
+            idleChecker += Time.fixedDeltaTime;
+
+            if (idleChecker > 0.1f) // 단위 초
+                isRun = false;
+            else
+                isRun = true;
+
             // 위쪽, 오른쪽 화살표가 같이 눌려져 있을 때(오른쪽위로 점프)
             if(Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.RightArrow) && heroKnightRigidbody.velocity.y == 0)
             {
@@ -44,17 +53,27 @@ public class HeroKnightController : MonoBehaviour
             {
                 heroKnightRigidbody.AddForce(transform.up * jumpForce);
             }
+            // 오른쪽 방향키 뗐을 땐 다시 Idle 애니메이션을 재생한다.
+            else if (Input.GetKeyUp(KeyCode.RightArrow))
+                isRun = false;
+            // 왼쪽 방향키 뗐을 땐 다시 Idle 애니메이션을 재생한다.
+            else if (Input.GetKeyUp(KeyCode.LeftArrow))
+                isRun = false;
             // 오른쪽 방향키 입력했을 때(우측무빙)
             else if (Input.GetKey(KeyCode.RightArrow) && heroKnightRigidbody.velocity.y == 0)
             {
-                heroKnightRigidbody.MovePosition(heroKnightRigidbody.position + horizontalMoveForce * Time.fixedDeltaTime);
+                idleChecker = 0;
+                heroKnightRigidbody.MovePosition(heroKnightRigidbody.position + horizontalMoveForce * Time.fixedDeltaTime);   
             }
-            else if (Input.GetKey(KeyCode.LeftArrow) && heroKnightRigidbody.velocity.y == 0) // 왼쪽 방향키 입력
+            // 왼쪽 방향키 입력했을 때(좌측무빙)
+            else if (Input.GetKey(KeyCode.LeftArrow) && heroKnightRigidbody.velocity.y == 0)
             {
-                heroKnightRigidbody.MovePosition(heroKnightRigidbody.position + horizontalMoveForce * -1 * Time.fixedDeltaTime);
-            }
+                idleChecker = 0;
+                heroKnightRigidbody.MovePosition(heroKnightRigidbody.position + horizontalMoveForce * -1 * Time.fixedDeltaTime);                
+            }      
         }
-
+        
+        heroKnightAnimator.SetBool("isRun", isRun); // heroKnight의 애니메이션 값을 계속해서 갱신
         heroKnightAnimator.SetBool("isGrounded", isGrounded); // heroKnight의 애니메이션 값을 계속해서 갱신
     }
 
