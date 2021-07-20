c109b684d16e312fe9c1184b9aff0c96d9a8ed1d
seasunrise22 <seasunrise22@gmail.com>
2021-07-20 23:07:03 +0900

    중력 추가, 코드 정리

--- Assets/Scripts/HeroKnightController.cs
+++ Assets/Scripts/HeroKnightController.cs
@@ -3,20 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HeroKnightController : MonoBehaviour
-{    
+{
+    /* 상태변수 */
     private bool isDead = false;        // 사망여부    
     private bool isGrounded = false;    // 캐릭터가 점프했을 때 애니메이터에 Set시킬 점프 상태.
     private bool isRun = false;         // 캐릭터를 좌우로 조작했을 때 애니메이터에 Set시킬 달리기 상태.
-    private float jumpForce = 350f;     // 위쪽 방향키 입력했을 때 캐릭터의 위쪽으로 작용할 힘.
-    private float idleChecker;           // 아무것도 입력하지 않았을 때의 행동(달리기->Idle)을 결정짓기 위한 타이머
-    private Vector2 upRightForce = new Vector2(350f, 300f);     // 오른쪽위로 점프할 때 가할 힘
-    private Vector2 upLeftForce = new Vector2(-350f, 300f);     // 왼쪽위로 점프할 때 가할 힘    
-    private Vector2 horizontalMoveForce = new Vector2(8.0f, 0);   
 
+    /* 타이머 */
+    private float idleChecker;          // 아무것도 입력하지 않았을 때의 행동(달리기->Idle)을 결정짓기 위한 타이머
+
+    /* 캐릭터에 적용시킬 힘 */
+    private float jumpForce = 350f;     // 위쪽 방향키 입력했을 때 캐릭터의 위쪽으로 작용할 힘.
+    private Vector2 upRightForce = new Vector2(250f, 330f); // 오른쪽위로 점프할 때 가할 힘
+    private Vector2 upLeftForce = new Vector2(-250f, 330f); // 왼쪽위로 점프할 때 가할 힘    
+    private Vector2 walkForce = new Vector2(8.0f, 0);       // 좌우 이동 할때의 힘     
+    private Vector2 gravityForce = new Vector2(0f, -9.8f);  // 캐릭터가 낙하할 때 적용시킬 중력.
+
+    /* 컴포넌트 접근용 */
     private Rigidbody2D heroKnightRigidbody;    // 리지드바디 접근용    
     private Animator heroKnightAnimator;        // 애니메이터 접근용
 
-    // Start is called before the first frame update
     void Start()
     {
         heroKnightRigidbody = GetComponent<Rigidbody2D>();
@@ -31,6 +37,7 @@ public class HeroKnightController : MonoBehaviour
         }
         else
         {
+            // Idle 상태인지 아닌지 체크
             idleChecker += Time.fixedDeltaTime;
 
             if (idleChecker > 0.1f) // 단위 초
@@ -38,38 +45,38 @@ public class HeroKnightController : MonoBehaviour
             else
                 isRun = true;
 
-            // 위쪽, 오른쪽 화살표가 같이 눌려져 있을 때(오른쪽위로 점프)
+            // 캐릭터가 점프 후 낙하하고 있을 때 좀 더 빨리 떨어지도록 하기 위한 코드
+            if(heroKnightRigidbody.velocity.y < 0)
+            {
+                heroKnightRigidbody.AddForce(gravityForce);
+            }
+
+            // '오른쪽+위' 입력
             if(Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.RightArrow) && heroKnightRigidbody.velocity.y == 0)
             {
                 heroKnightRigidbody.AddForce(upRightForce);
             }
-            // 위쪽, 왼쪽 화살표가 같이 눌려져 있을 때(왼쪽위로 점프)
+            // '왼쪽+위' 입력
             else if (Input.GetKey(KeyCode.UpArrow) && Input.GetKey(KeyCode.LeftArrow) && heroKnightRigidbody.velocity.y == 0)
             {
                 heroKnightRigidbody.AddForce(upLeftForce);
             }
-            // 위쪽 방향키 입력했을 때(수직점프)
+            // '위쪽' 입력
             else if (Input.GetKeyDown(KeyCode.UpArrow) && heroKnightRigidbody.velocity.y == 0) 
             {
                 heroKnightRigidbody.AddForce(transform.up * jumpForce);
             }
-            // 오른쪽 방향키 뗐을 땐 다시 Idle 애니메이션을 재생한다.
-            else if (Input.GetKeyUp(KeyCode.RightArrow))
-                isRun = false;
-            // 왼쪽 방향키 뗐을 땐 다시 Idle 애니메이션을 재생한다.
-            else if (Input.GetKeyUp(KeyCode.LeftArrow))
-                isRun = false;
-            // 오른쪽 방향키 입력했을 때(우측무빙)
+            // '오른쪽' 입력
             else if (Input.GetKey(KeyCode.RightArrow) && heroKnightRigidbody.velocity.y == 0)
             {
                 idleChecker = 0;
-                heroKnightRigidbody.MovePosition(heroKnightRigidbody.position + horizontalMoveForce * Time.fixedDeltaTime);   
+                heroKnightRigidbody.MovePosition(heroKnightRigidbody.position + walkForce * Time.fixedDeltaTime);   
             }
-            // 왼쪽 방향키 입력했을 때(좌측무빙)
+            // '왼쪽' 입력
             else if (Input.GetKey(KeyCode.LeftArrow) && heroKnightRigidbody.velocity.y == 0)
             {
                 idleChecker = 0;
-                heroKnightRigidbody.MovePosition(heroKnightRigidbody.position + horizontalMoveForce * -1 * Time.fixedDeltaTime);                
+                heroKnightRigidbody.MovePosition(heroKnightRigidbody.position + walkForce * -1 * Time.fixedDeltaTime);                
             }      
         }
         
